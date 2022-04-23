d630680965b2a169f26306bf41abc20c916852de
ShivalilaPatil <shivalilapatil66@gmail.com>
2022-04-23 04:02:07 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { UsersdataService } from './services/usersdata.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app7';
+  users: any;
+  constructor(private userdata: UsersdataService) {
+    this.userdata.users().subscribe((data) => {
+      console.warn('data', data);
+      this.users = data;
+    });
+  }
+  getUserFormData(data: any) {
+    this.userdata.SaveUsers(data).subscribe((result) => {
+      console.warn(result);
+    });
+  }
 }
